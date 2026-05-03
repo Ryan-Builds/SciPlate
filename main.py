54404a07911f077d21e537f1beb57657742a103a
Ryan-19-creator <hamidreza.rayani@gmail.com>
2026-05-03 10:41:48 +0200

    added Branded foods

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from datetime import datetime
 #loading my API key from the config file
 load_dotenv("config.env")
 #For my own use I will use the following API key 
-my_usda_api_key = os.getenv("usda_api_key")
+#my_usda_api_key = os.getenv("usda_api_key")
 
 #for public testing a demo API key is available:
-#my_usda_api_key = "DEMO_KEY"
+my_usda_api_key = "DEMO_KEY"
 
 #this is the base url from the usda webite, later in the function,
 #I will add the necessary missing characters
@@ -48,26 +48,35 @@ def usda_food_info_extractor(usda_data):
     #first step is to get into the food level
     first_level = usda_data['foods']
     foundation_data = []
-
+    branded_data = []
     #My decision is for now extract the data related to foundation:
     #I will just loop through the first_level variable just created
     #I will add each dictionary that is inside the foundation datatype
 
-    for item in first_level:
-        if item['dataType'] == 'Foundation':
-            foundation_data.append(item)
+    for item1 in first_level:
+        if item1['dataType'] == 'Foundation':
+            foundation_data.append(item1)
 
-    #ok let's try to show the user all the found matches, he will choose the number
-    #and based on the chosen number we can drilldown into the data
-    if len(foundation_data)==0:
+    #Here I will add a loop to extract Branded foods from the database, it is necessary for many items like pizza!
+    
+    for item2 in first_level:
+        if item2['dataType'] == 'Branded':
+            branded_data.append(item2)
+
+    if len(foundation_data)==0 and len(branded_data)==0 :
         print("Sorry I was not able to find any match for the food item :(, Do you want to try again with another item or exit?")
         users_response = input(". Please select (y or n):")
         if users_response.lower()[0] == "y":
             main_app()
         elif users_response.lower()[0] == "n":
             return 0
-            
-    else:
+    #I will choose one datatype and work with it. For now I will choose the foundation data as my priority. 
+    #I will use an else if with foundation data as the priority, because in cas the found data is branded I need t show an info that is different
+
+    #ok let's try to show the user all the found matches, he will choose the number
+    #and based on the chosen number we can drilldown into the data
+
+    elif len(foundation_data) != 0:
         print(f"Good news, I have found {len(foundation_data)} matches \n")
         for i, j in enumerate(foundation_data):
             print(f"item number {i}: {j['description']}")
@@ -78,27 +87,39 @@ def usda_food_info_extractor(usda_data):
         #let's drilldown to the key as well
 
         chosen_item_nutrients = chosen_item['foodNutrients']
+
+    elif len(branded_data) != 0:
+        print(f"Good news, I have found {len(branded_data)} matches \n")
+        for i, j in enumerate(branded_data):
+            print(f"""item number {i}: {j['description']}, Brand: {j['brandName'] if j.get('brandName') else 'Brand name not available'}, Market Country: {j['marketCountry']}, Ingredients: {j['ingredients']}""")
+        chosen_number = int(input("\nPlease choose your desired item number: "))
+        chosen_item = branded_data[chosen_number]
+
+        # the chosen food item here is now a dictionary, the key we need is called "foodNutrients"
+        #let's drilldown to the key as well
+
+        chosen_item_nutrients = chosen_item['foodNutrients']
         
         #ok at this point user has chosen the desired item, we can drill down
         #I will extract and return the nutrition values
         #calory finder:
-        for info in chosen_item_nutrients:
-            if (info['unitName'] == 'KCAL') and (info['nutrientNumber'] == '208'):
-                calorie = info['value']
-            elif (info['unitName'] =='KCAL') and (info['nutrientNumber'] == '957'):    	
-                calorie = info['value']
-        #protein finder:
-            elif (info['nutrientName'] == 'Protein') and (info['nutrientNumber'] == '203'):
-                protein = info['value']
-        #carb finder:
-            elif ('Carbohydrate' in info['nutrientName'] ) and (info['nutrientNumber'] == '205'):
-                carbs = info['value']
-        #fat finder:
-            elif ('fat' in info['nutrientName'] ) and (info['nutrientNumber'] == '204'):
-                fat = info['value']
+    for info in chosen_item_nutrients:
+        if (info['unitName'] == 'KCAL') and (info['nutrientNumber'] == '208'):
+            calorie = info['value']
+        elif (info['unitName'] =='KCAL') and (info['nutrientNumber'] == '957'):    	
+            calorie = info['value']
+    #protein finder:
+        elif (info['nutrientName'] == 'Protein') and (info['nutrientNumber'] == '203'):
+            protein = info['value']
+    #carb finder:
+        elif ('Carbohydrate' in info['nutrientName'] ) and (info['nutrientNumber'] == '205'):
+            carbs = info['value']
+    #fat finder:
+        elif ('fat' in info['nutrientName'] ) and (info['nutrientNumber'] == '204'):
+            fat = info['value']
 
 
-        return {'Calorie':calorie, 'Protein':protein, 'Carbs': carbs, 'Fat': fat}
+    return {'Calorie':calorie, 'Protein':protein, 'Carbs': carbs, 'Fat': fat}
 
 
 def main_app():
